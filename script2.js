ca5ba1e3977e3bb308df1df15f19cf220149a958
saniahsafdar <saniahsafdar@gmail.com>
2021-04-21 14:19:25 -0400

    added timer span to HTML

--- script2.js
+++ script2.js
@@ -1,7 +1,11 @@
+let hour = 0;
+let minute = 0;
+let second = 0;
+
 const timeStart = document.getElementById("start");
 
 timeStart.addEventListener("click", ()=>{
     setTimeout(() => {
         document.body.classList.toggle("switch");
-    }, 3000);
+    }, 1000); // 1000ms = 1s
 })
