d9e49bf939723a169380f3c71e52a4d1de468a2d
Dair Smith <dair.code@gmail.com>
2021-04-21 09:07:26 -0400

    added comments in js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// array of images to be inserted into HTML
 let cardImgs = ['<img src="images/picture1.jpg" />', '<img src="images/picture2.jpg" />', '<img src="images/picture3.jpg" />', '<img src="images/picture4.jpg" />', '<img src="images/picture5.jpg" />', '<img src="images/picture6.jpg" />', '<img src="images/picture7.jpg" />', '<img src="images/picture8.jpg" />', '<img src="images/picture9.jpg" />', '<img src="images/picture1.jpg" />', '<img src="images/picture2.jpg" />', '<img src="images/picture3.jpg" />', '<img src="images/picture4.jpg" />', '<img src="images/picture5.jpg" />', '<img src="images/picture6.jpg" />', '<img src="images/picture7.jpg" />', '<img src="images/picture8.jpg" />', '<img src="images/picture9.jpg" />'];
 
+// function to shuffle the order of the image array
 function shuffleCards(array) {
     var currentIndex = array.length, tempValue, rndmNum;
 
