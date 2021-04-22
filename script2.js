cefcd7706942259ece1242e03ec5d79f3df84095
saniahsafdar <saniahsafdar@gmail.com>
2021-04-22 07:35:06 -0400

    tried to add clearInterval to the JS file

--- script2.js
+++ script2.js
@@ -1,20 +1,26 @@
 
+// grabbing 'start' and 'reset' buttons from html
 const start = document.getElementById("start");
 const reset = document.getElementById("reset");
 
+// adjusting the labels for seconds/minutes in the html
 var minutesLabel = document.getElementById("minutes");
 var secondsLabel = document.getElementById("seconds");
 var totalSeconds = 0;
+var myVar = setTime();
+
+//when you click on start, this well set the timer to start going.
 start.onclick = () => setInterval(setTime, 1000);
-reset.onclick = () => playing = false;
+reset.onclick = () => stopPlaying;
 let playing;
 
+
+//setting the time to add seconds, then when reaching 60 seconds, start adding minutes.
 function setTime() {
   {++totalSeconds;
   secondsLabel.innerHTML = pad(totalSeconds % 60);
   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));} 
 }
-
 
 function pad(val) {
   var valString = val + "";
@@ -23,4 +29,8 @@ function pad(val) {
   } else {
     return valString;
   }
+}
+//clearing the interval to stop the timer when pressing reset.
+function stopPlaying(){
+    clearInterval(myVar)
 }
