539f4ee412e08c79cc4b536777759f93fdb35e3b
Amber Holland <Amber.S.Creech@gmail.com>
2021-04-23 09:14:36 -0400

    uncommented stuff

--- script.js
+++ script.js
@@ -39,22 +39,22 @@ function shuffleCards(array) {
         array[currentIndex] = array[rndmNum];
         array[rndmNum] = tempValue;
     }
-    showAllCards();
+    return array
 };
 
 // Shows shuffled cards for a brief moment after player pushed start ********************* on start button
-function showAllCards(card) {
-    for (card in cards) {
-        console.log(cards[card]);
-        for (cardImg in cardImgs) {
-            cards[card].classList.toggle('flipCard');
-            setTimeout(() => {
-                cards[card].src = cardImg;
-            }, 100)
-        }
+// function showAllCards(card) {
+//     for (card in cards) {
+//         console.log(cards[card]);
+//         for (cardImg in cardImgs) {
+//             cards[card].classList.toggle('flipCard');
+//             setTimeout(() => {
+//                 cards[card].src = cardImg;
+//             }, 100)
+//         }
 
-    }
-}
+//     }
+// }
 
 // cards.forEach(function (currentValue, currentIndex, listObj) {
 //     console.log(currentValue + ', ' + currentIndex + ', ' + this);
